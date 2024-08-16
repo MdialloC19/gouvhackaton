47a7d8e01d744f893c970d9d2ee4f4da2dcee163
MdialloC19 <mdiallo3211@gmail.com>
2024-08-16 21:24:22 +0000

    config : Set up for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule, MongooseModule.forRoot(process.env.MONGO_URI)],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
